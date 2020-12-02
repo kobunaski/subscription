ffdaeeadbc349790c95612d0b77b413067ad9f19
Giang Vu <vuphamtruonggiang20@gmail.com>
2020-12-02 09:20:10 +0700

    v1.0.6

--- src/controllers/SubscriptionController.php
+++ src/controllers/SubscriptionController.php
@@ -85,6 +85,6 @@ class SubscriptionController extends Controller
         $subscription->email = $email;
         $subscription->save();
 
-        $this->refresh();
+        return $this->redirect(Craft::$app->request->referrer ?: Craft::$app->homeUrl);
     }
 }
